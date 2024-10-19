df4c1c6e2311998f6a847a4d309774782aae0e56
vgalovic <vgalovic99@gmail.com>
2024-10-20 00:48:11 +0200

    change to yazi plugin

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -1,5 +1,4 @@
 require("starship"):setup({ config_file = "$HOME/.config/yazi/starship_yazi.toml" })
-require("eza-preview"):setup()
 require("git"):setup()
 require("full-border"):setup({
 	-- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
